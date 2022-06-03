58a57e9482ab94d864551c74b809d23276de0b04
Mikias Wondyfraw <mikiaswondyfraw@gmail.com>
2022-06-03 09:09:01 -0500

    new changes

--- js/script.js
+++ js/script.js
@@ -14,24 +14,24 @@ Add the startIndex & endIndex to the function
 function showPage(list, page) {
    let startIndex = (page  * 9) - 9;
    let endIndex = (page * 9);
-   let studentList = document.getElementByClassName('.student-list');
+   let studentList = document.getElementsByClassName('student-list')[0];
    
    studentList.innerHTML= '';
 
    for (let i = 0; i < list.length; i++) {
-      if ( i >= startIndex && < endIndex) {
+      if ( i >= startIndex && i < endIndex) {
 
       let studentItem=  
-      <li class="student-item cf">
+      `<li class="student-item cf">
     <div class="student-details">
-      <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-      <h3>${list[i].name.first} ${list[i]. name. list}</h3>
+      <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+      <h3>${list[i].name.first} ${list[i]. name. last}</h3>
       <span class="email">${list [i]. email}</span>
     </div>
     <div class="joined-details">
       <span class="date">${list [i]. registered. date}</span>
     </div>
-  </li>
+  </li>`
       studentList.insertAdjacentHTML('beforeend',studentItem);
       
       }
@@ -44,24 +44,22 @@ This function will create and insert/append the elements needed for the paginati
 */
 function addPagination(list) {
    let numofPages = Math.ceil(list.length / 9);
-   let linkList= document.getElementByClassName('.link-list');
-
-   linkList.innerHTML = '';
-
+   let linkList= document.querySelector('link-list');
+   addPagination.innerHTML = "linkList"
    for (let i = 1; i <= numofPages; i++) {
-      button = 
-     <li> <button type="button">${i}</button></li>
-      linkList.insertAdjacentHTML('beforeend',button);
-      firstButton = document.querySelector('active');
-      button.className = 'active';
+      button = `<li><button type="button">${i}</button></li>`;
+       linkList.insertAdjacentHTML ('beforeend',button);
+      firstButton = document.querySelector('button');
+      firstButton.className = 'active';
       linkList.addEventListener('click', (e) => {
          if (e.target.tagName ==='Button') {
-         document.querySelector('active').className = ''; 
-         activeButton.className = 'active';
+         activeButton = document.querySelector('active');
+         activeButton.className = ''; 
+         e.target.className = 'active';
          showPage(list, e.target.textContent);
 
          }
-      }
+      });
    }
 
 }
